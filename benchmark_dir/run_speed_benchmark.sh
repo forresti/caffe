a915c82ba195c7b9015257bfe9161d0e68c539ed
forresti <fiandola@gmail.com>
2014-03-07 14:07:37 -0800

    minor changes to scripts

--- benchmark_dir/run_speed_benchmark.sh
+++ benchmark_dir/run_speed_benchmark.sh
@@ -1,6 +1,7 @@
 
 
-../build/examples/net_speed_benchmark_forrest.bin ./imagenet_val.prototxt
-#../build/examples/net_speed_benchmark_forrest.bin ./imagenet_rcnn_batch_1_input_2000x2000_output_conv5.prototxt
+#../build/examples/net_speed_benchmark_forrest.bin ./imagenet_val.prototxt
+../build/examples/net_speed_benchmark_forrest.bin ./imagenet_rcnn_batch_1_input_2000x2000_output_conv5.prototxt
 #../build/examples/net_speed_benchmark_forrest.bin ../python/caffe/imagenet/imagenet_rcnn_batch_1_input_2000x2000_output_conv5_lowStride.prototxt
+#../build/examples/net_speed_benchmark_forrest.bin ./imagenet_rcnn_batch_1_input_2000x2000_output_fc6.prototxt
 
